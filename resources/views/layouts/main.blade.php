1a4a8fba827a95fc0922e29b53975042d76d9b78
mesaque <mesaque1212@gmail.com>
2022-06-24 23:15:15 -0300

    Passando o caminho no main.blade

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,7 +28,7 @@
                                 <a href="/" class="nav-link">Eventos</a>
                             </li>
                             <li class="nav-item">
-                                <a href="/" class="nav-link">Criar Eventos</a>
+                                <a href="/events/create" class="nav-link">Criar Eventos</a>
                             </li>
                             <li class="nav-item">
                                 <a href="/" class="nav-link">Entrar</a>
@@ -44,5 +44,6 @@
     <footer>
         <p>HDC Events &copy; 2022</p>
     </footer>
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
     </body>
 </html>
