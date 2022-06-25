37134966815e3e573efbc0b268b541d251d92cc5
mesaque <mesaque1212@gmail.com>
2022-06-24 23:14:12 -0300

    Criando as rotas

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+use App\Http\Controllers\EventController;
 
-    $nome = "Mesaque";
-    $idade = 18;
-    $profissao = "Desenvolvedor Web";
-
-    $array = [100,200,300,400,500];
-    $nomes = ['Mesaque','Lucas','Victor', 'Marco'];
-
-    return view('welcome', [
-        'nome' => $nome,
-        'idade' => $idade,
-        'profissao' => $profissao,
-        'array' => $array,
-        'nomes' => $nomes
-    ]);
-});
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
 
 Route::get('/contato', function() {
     return view('contato');
