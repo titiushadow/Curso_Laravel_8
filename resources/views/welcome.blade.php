d1d10fe77933ed9fd58be4ebe6915ce734258225
mesaque <mesaque1212@gmail.com>
2022-06-25 18:01:41 -0300

    Finalizando a home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,34 @@
 @section('title', 'HDC Events')
 
 @section('content')
-    @foreach ( $events as $event )
-        <p>{{ $event->title }} -- {{ $event->description }} </p>
-    @endforeach
+
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
+        </form>
+    </div>
+    <div id="events" class="container col-md-12">
+        <h2>Proximos Eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach ($events as $event )
+                <div class="card col-md-3">
+                    <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                    <div class="card-body">
+                        <p class="card-date">
+                            25/06/2022
+                        </p>
+                        <h5 class="card-title">
+                            {{ $event->title }}
+                        </h5>
+                        <p class="card-participantes">
+                            X Participantes
+                        </p>
+                        <a href="#" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
