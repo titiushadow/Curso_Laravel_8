ba8d0e4ffbb4499ef71189362ef87364d8f5da18
mesaque <mesaque1212@gmail.com>
2022-06-26 00:01:10 -0300

    Passando a flash message para home

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,7 +40,16 @@
                 </div>
             </nav>
         </header>
-    @yield('content')
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @If(session('msg'))
+                    <p class="msg">{{ session('msg') }}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
     <footer>
         <p>HDC Events &copy; 2022</p>
     </footer>
